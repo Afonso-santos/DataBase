5d7464b7f2a8cbbf8a5629f4dc959b3a83483320
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-05-28 22:39:38 +0100

    Update index_DML.sql

--- sql/index_DML.sql
+++ sql/index_DML.sql
@@ -1,3 +1,3 @@
 CREATE INDEX idx_Caso ON Evidência (Caso);
-CREATE INDEX idx_telefone ON Detetive(telefone);
-CREATE INDEX idx_dataDesvinculação  ON Vinculado(dataDEsvinculação);
+CREATE INDEX idx_telefone ON Detetive (Telefone);
+CREATE INDEX idx_dataDesvinculação ON Vinculado (DataDesvinculação);
