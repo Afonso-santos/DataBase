a619a6234ff122a2cd39947ed3882126c56bcbb8
migueltc13 <73550106+migueltc13@users.noreply.github.com>
2024-05-28 20:46:43 +0100

    Update procedures_DQL.sql

--- sql/procedures_DQL.sql
+++ sql/procedures_DQL.sql
@@ -1,22 +1,17 @@
-USE CDC;
-
 -- R11: Os dados relativos de cada caso - evidências, suspeitos e testemunhas - devem ser apresentados por ordem cronológica.
 DELIMITER //
 CREATE PROCEDURE RelatorioCronologico(IN caso_id INT)
 BEGIN
-    -- Selecionar evidências do caso
     SELECT 'Evidência' AS Tipo, e.ID AS ID, e.DataColeta AS Data, e.Descrição AS Descrição, e.Arquivo AS Arquivo FROM Evidência AS e
 		WHERE e.Caso = caso_id
 	    UNION ALL
 
-    -- Selecionar suspeitos do caso
     SELECT 'Suspeito' AS Tipo, s.ID AS ID, NULL AS Data, s.Nome AS Descrição, NULL AS Arquivo FROM Suspeito AS s
 		INNER JOIN CasoSuspeito AS cs
 		ON (s.ID = cs.Suspeito)
 		WHERE cs.Caso = caso_id
         UNION ALL
 
-    -- Selecionar testemunhas do caso
     SELECT 'Testemunha' AS Tipo, t.ID AS ID, NULL AS Data, t.Nome AS Descrição, NULL AS Arquivo FROM Testemunha AS t
 		INNER JOIN CasoTestemunha AS ct
         ON (t.ID = ct.Testemunha)
@@ -38,7 +33,7 @@ END //
 DELIMITER ;
 
 -- R13: O sistema permite a pesquisa de características comuns entre casos,
--- através da pesquisa de descrições nas seguintes entidades: casos, evidências, suspeitos e testemunhas.
+-- através da pesquisa de descrições nas seguintes tabelas: Caso, Evidência e Suspeito.
 DROP PROCEDURE IF EXISTS PesquisaCaracteristicasComuns;
 DELIMITER //
 CREATE PROCEDURE PesquisaCaracteristicasComuns (IN Input TEXT)
@@ -51,7 +46,6 @@ CREATE PROCEDURE PesquisaCaracteristicasComuns (IN Input TEXT)
         WHERE Descrição LIKE Input;
 END //
 DELIMITER ;
--- CALL PesquisaCaracteristicasComuns('1');
 
 -- R21: Os dados relativos a um detetive devem ser acedidos através do seu identificador único.
 DELIMITER //
@@ -83,12 +77,3 @@ BEGIN
         WHERE c.Cliente = cliente_id;
 END //
 DELIMITER ;
-
--- R49: No encerramento de cada dia, o sistema deverá gerar um relatório que inclua todas as despesas e pagamentos efetuados.
--- Este deve apresentar individualmente cada despesa e pagamento, se existirem, por caso. Adicionalmente, o relatório deve fornecer
--- o somatório total de despesas, pagamentos, bem como os lucros ou prejuízos acumulados nesse dia.
-
--- R50: No encerramento de cada dia, o sistema deverá gerar um relatório para cada caso que inclua novas evidências, testemunhas e suspeitos.
-
--- R51: No encerramento de cada semana, o sistema deverá gerar um relatório com informações e estatísticas relativas a novos casos abertos, fechados e/ou arquivados.
-
